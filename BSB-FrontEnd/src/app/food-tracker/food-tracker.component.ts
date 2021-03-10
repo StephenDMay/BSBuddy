742e01de60b4d59dcf8074a7240843903b64f82c
Stephen May <stephendouglasm@gmail.com>
2021-03-10 16:32:53 -0500

    Created dynamic food tracking table

--- BSB-FrontEnd/src/app/food-tracker/food-tracker.component.ts
+++ BSB-FrontEnd/src/app/food-tracker/food-tracker.component.ts
@@ -17,49 +17,56 @@ export class FoodTrackerComponent implements OnInit {
   carbs: number;
   name: string;
 
+  
+
   constructor(private service : BsManagerService, private router : Router) { }
 
   ngOnInit(): void {
     
   }
 
+  
+
   findFood(): void {
   
     // add reset function if needed
   
     let foodName = $("#search").val();
+    
 
     $.get(
         `https://api.nal.usda.gov/fdc/v1/foods/search?api_key=SohihGmRVzAtcLPY0c3Yo2SQsWE8V2992IbSe4VT&query=${foodName}`,
         function (data, textStatus, jqXHR) {
             let carbs = `${data.foods[0].foodNutrients[2].value}`;
             
-            document.getElementById("carbs").textContent = carbs;
+            // document.getElementById("carbs").textContent = carbs;
             console.log(data);
             
             let carbNum = parseFloat(carbs);
             total += carbNum;
-            let output = total.toString();
-            document.getElementById("totalCarbs").textContent = output;
             let name = foodName.toString();
-            let foodItem = document.createElement("span");
-            let carbCount = document.createElement("span");
-            let br = document.createElement("br");
-            foodItem.textContent = name;
-            carbCount.textContent = " | " + carbs;
-            document.getElementById("list").appendChild(br);
-            document.getElementById("list").appendChild(foodItem);
-            document.getElementById("list").appendChild(carbCount);
-            document.getElementById("list").appendChild(br);
-            
-            
-            
+
+            document.getElementById("tableBody").innerHTML += `
+              <tr>
+                <td>${name}</td>
+                <td>${carbs}</td>
+              </tr>
+            `;
+
+            document.getElementById("tableFoot").innerHTML = `
+            <tr>
+              <th> Total </th>
+              <td> ${total} </td>
+            </tr>
+            `
             
             
         }
     );
   
   }
+
+  
 
   addMeal(totalCarbs : string, label : string, ) {
     console.log(totalCarbs);
